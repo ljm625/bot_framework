fb3c574e77cf20fc39062d29af1343c7238365d1
Jiaming Li <jiaminli@cisco.com>
2016-11-23 17:12:47 -0800

    update the address for syntaxnet

--- config.py
+++ config.py
@@ -7,7 +7,7 @@
 # @Software: PyCharm
 
 
-SYNTAXNET_IP='http://127.0.0.1:9000'
+SYNTAXNET_IP='http://vm-bench-31:9000'
 SPARK_TOKEN='MDRlNjhiNjktOGM2Mi00Mjk4LTk5MzYtOWRjZmU1ZWVlNzY4ZjJkMTg3NDItOWUw'
 SPARK_URL='https://api.ciscospark.com/v1/'
 BOT_ACCOUNT='aire@sparkbot.io'
